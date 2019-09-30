b6d9bb70d29e39bf032cf7583ec65d0e2ce796e5
Richard Webb <rdw@md1tech.co.uk>
2019-09-30 20:42:34 +0100

    added udev to make rudatctrl not need root access and updated install script to add rules to udev

--- install.sh
+++ install.sh
@@ -5,3 +5,4 @@
 sudo apt-get install libusb-dev libusb-1.0.0-dev libudev-dev libhidapi-dev libserial-dev
 g++ -std=c++11 -o rudatctrl -g main.cpp rudat_6000_90_attenuator.cpp -lusb -lhidapi-hidraw
 sudo cp rudatctrl /usr/local/bin
+sudo cp 85-rudat.rules /etc/udev/rules.d/
